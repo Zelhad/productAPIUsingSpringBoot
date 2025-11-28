b5345446a4d959dfa21707ea8251ab3a48424cc4
Zelhad <ze.elhadraoui@gmail.com>
2025-11-28 20:30:14 +0100

    Repository added

---
+++ src/main/java/com/productApi/productApi/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.productApi.productApi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.productApi.productApi.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+	
+
+}
