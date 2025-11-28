9837ad4425681dbe0377accfec119e59ebe9cf19
Zelhad <ze.elhadraoui@gmail.com>
2025-11-28 17:05:36 +0100

    remove system.out.println  from the  main SpringBoot

--- src/main/java/com/productApi/productApi/ProductApiApplication.java
+++ src/main/java/com/productApi/productApi/ProductApiApplication.java
@@ -8,7 +8,7 @@ public class ProductApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiApplication.class, args);
-		System.out.println("Finally I meed  you Mister Spring boot");
+		
 	}
 
 }
